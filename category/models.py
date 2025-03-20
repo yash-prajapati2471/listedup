4c27d36af5f3b697ee7d09f2d9dd3edcaf49f556
Yash <yp8404661@gmail.com>
2025-03-20 10:20:43 +0530

    23 commit

--- category/models.py
+++ category/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.urls import reverse
-from cloudinary.models import CloudinaryField
 # Create your models here.
 
 class Category(models.Model):
     category_name = models.CharField(max_length=255,unique=True)
     slug = models.SlugField(max_length=200,unique=True)
-    image = CloudinaryField('category_image')
+    image = models.ImageField(upload_to='category_image')
 
     def __str__(self):
         return self.category_name
